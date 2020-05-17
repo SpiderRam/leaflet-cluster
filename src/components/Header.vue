a650c7d7c00b35553c25f96020691b6cab3f0062
Carmen Owen <doctorsout@yahoo.com>
2020-05-16 22:06:22 -0400

    Use flexbox in header for mobile responsiveness

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -28,7 +28,8 @@ export default {
     text-shadow: 1px 1px 2px black, 0 0 25px lemonchiffon, 0 0 5px lawngreen;
     background-size: cover;
     width: 100vw;
-    height: 230px;
+    display: flex;
+    flex: row nowrap;
 }
 
 .title {
--- src/components/Title.vue
+++ src/components/Title.vue
@@ -1,39 +1,44 @@
 <template>
-  <div>
-    <p class="title">All Starbucks Locations in the State of Colorado.</p>
-    <p class="subtitle">Select the 'Clusters' view for a more user-friendly map.</p>
-  </div>
+    <div>
+        <p class="title">All Starbucks Locations in the State of Colorado.</p>
+        <p class="subtitle close">
+            (Well, pretty close; I'm sure there are more now....)
+        </p>
+        <p class="subtitle">
+            Select the 'Clusters' view for a more user-friendly map.
+        </p>
+    </div>
 </template>
 
 <script>
 export default {
-  name: 'app',
-  data () {
-    return {
-
-    }
-  },
-  methods: {
-
-  }
-}
+    name: 'app',
+    data() {
+        return {};
+    },
+    methods: {},
+};
 </script>
 
 <style scoped>
-
-  div {
+div {
     margin: 0 auto;
     width: 70%;
     text-align: center;
     font-family: 'Gaegu', cursive;
-  }
+}
 
-  .title {
+.title {
     font-size: 3em;
-  }
+    margin-bottom: 5px;
+}
 
-  .subtitle {
+.subtitle {
     font-size: 2em;
-  }
+}
 
+.close {
+    margin-top: 10px;
+    font-size: 1.5em;
+}
 </style>
